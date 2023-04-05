8548ae5198ffaecc0856e0619de72cc81abdcca9
NaughtySloth <filipzelenik@gmail.com>
2023-04-05 12:19:27 +0200

    Add project files.

---
+++ service1/entrypoint.py
@@ -0,0 +1,31 @@
+"""
+import sys
+import hashlib
+
+inp = sys.stdin.readlines()
+hash_func = inp[0].strip()
+message = '\n'.join(inp[1:]).strip()
+
+h = hashlib.new(hash_func)
+h.update(str.encode(message))
+
+print h.hexdigest()
+"""
+
+# the old code is written in Python2 which is deprecated so I've upgraded it to v3 and also made some other improvements to make the code more readable and robust
+# it also reads the input from stdin which I've replaced with reading from the request from the other service passed by fwatchdog
+import sys
+import hashlib
+
+def handle(req):
+    # shorter version of the code which uses .map to avoid loops
+    hash_func, *messages = map(str.strip, req.split('\n'))
+    message = '\n'.join(messages)
+
+    # added hash validation before creating the hashlib object
+    if hash_func not in hashlib.algorithms_guaranteed:
+        return "Invalid hash function: {}".format(hash_func)
+    else:
+        h = hashlib.new(hash_func)
+        h.update(message.encode())
+        return h.hexdigest()
---
+++ service2/entrypoint.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+SERVICE1_URL = "http://service.example.com:8080"
+WEB_PAGE_URL = "https://www.google.com/"
+
+#message = requests.get(sys.stdin.readline()).text
+# the line above reads the string from the keyboard and not from a website as per the instructions
+# so I've changed it to read from a website instead and send that to the other service for hashing
+
+try:
+    response = requests.get(WEB_PAGE_URL)
+    web_page_content = response.text
+    
+    message = web_page_content
+    data = ["md5", message]
+
+    response = requests.post(SERVICE1_URL, data="\n".join(data))
+
+    print(response.text.strip())
+
+# added error handling which we need in case the website is unavailable or we don't have internet
+except requests.exceptions.RequestException as e:
+    print("Error: {}".format(e))
