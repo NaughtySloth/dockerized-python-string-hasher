b9dd9a22664f33af9085eefa6781ccd18fe019a3
NaughtySloth <filipzelenik@gmail.com>
2023-04-10 22:56:01 +0200

    added Flask to hasher service in order to keep the app running in docker until a request comes. added bash script to deploy both services

--- hashing-service/entrypoint.py
+++ hashing-service/entrypoint.py
@@ -15,18 +15,33 @@ print h.hexdigest()
 # the old code is written in Python2 which is deprecated so I've upgraded it to v3 and also made some other
 # improvements to make the code more readable and robust it also reads the input from stdin which I've replaced with
 # reading from the request from the other service passed by fwatchdog
+# also added Flask for definiting the port/REST method and for keeping the service running until requests from the web scraping service come
 import hashlib
+from flask import Flask, request
+
+app = Flask(__name__)
 
 
-def handle(req):
-    # shorter version of the code which uses .map to avoid loops
-    hash_func, *messages = map(str.strip, req.split('\n'))
-    message = '\n'.join(messages)
+@app.route('/', methods=['POST'])
+def handle():
+    req = request.data.decode('utf-8')
+    if request.method == 'POST':
+        # shorter version of the code which uses .map to avoid loops
+        hash_func, *messages = map(str.strip, req.split('\n'))
+        message = '\n'.join(messages)
 
-    # added hash validation before creating the hashlib object
-    if hash_func not in hashlib.algorithms_guaranteed:
-        return "Invalid hash function: {}".format(hash_func)
+        print(message)
+
+        # added hash validation before creating the hashlib object
+        if hash_func not in hashlib.algorithms_guaranteed:
+            return "Invalid hash function: {}".format(hash_func)
+        else:
+            h = hashlib.new(hash_func)
+            h.update(message.encode())
+            return h.hexdigest()
     else:
-        h = hashlib.new(hash_func)
-        h.update(message.encode())
-        return h.hexdigest()
+        return 'Unsupported method', 405
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ start_services.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage() {
+  echo "Usage: $0 --version-hashing-service <hashing-service-version> --version-web-scraping-service <web-scraping-service-version> [--additional-param-1 <value>] [--help|-h]"
+}
+
+OPTIONS=$(getopt -o 'h' -l version-hashing-service:,version-web-scraping-service:,additional-param-1:,help -- "$@")
+
+eval set -- "$OPTIONS"
+
+while true; do
+  case "$1" in
+    --version-hashing-service) version_hashing_service="$2"; shift 2;;
+    --version-web-scraping-service) version_web_scraping_service="$2"; shift 2;;
+    --additional-param-1) additional_param_1="$2"; shift 2;;
+    -h|--help) usage; exit 0;;
+    --) shift; break;;
+    *) echo "Invalid option: $1" >&2; usage; exit 1;;
+  esac
+done
+
+if [[ -z ${version_hashing_service:-} || -z ${version_web_scraping_service:-} ]]; then
+  echo "Missing arguments"
+  usage
+  exit 1
+fi
+
+if [[ -n ${additional_param_1:-} ]]; then
+  echo "Additional parameter 1: ${additional_param_1}"
+fi
+
+# Create a Docker network if it doesn't exist
+if ! docker network inspect my-network &>/dev/null; then
+  docker network create my-network
+fi
+
+# Start the containers in the same network
+docker run -d --name hashing-service --network my-network -p 8080:8080 naughtysloth/hashing-service:"${version_hashing_service}"
+docker run -d --name web-scraping-service --network my-network naughtysloth/web-scraping-service:"${version_web_scraping_service}"
+
+sleep 10
+read -p "Press Enter to continue..."
